ed43006be97c596034052078dc1871d481c420c3
Yapi théau <theau.yapi@ynov.com>
2022-12-08 12:58:15 +0100

    html de ****

--- main.go
+++ main.go
@@ -1,8 +1,26 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+)
+
+const port = ":8080"
+
+
+func Home(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Ouais, Champion, c, comment?")
+}
+
+func Play(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "tu, veux, jouer?")
+}
+
 
 func main() {
+	http.HandleFunc("/", Home)
+	http.HandleFunc("Play", Play)
 
-	fmt.Println("hello world")
+	fmt.Println("http://localhost:8080 - Server started on port", port)
+http.ListenAndServe(port, nil)
 }
