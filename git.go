45d3e5daaf67c386c73a416148a9250e6023aa14
Yapi théau <theau.yapi@ynov.com>
2023-01-05 09:49:40 +0100

    local host fonctionne avec index html

--- git.go
+++ git.go
@@ -2,47 +2,11 @@ package main
 
 import (
 	"fmt"
-	"html/template"
 	"net/http"
-	"path"
-	"time"
 )
 
-func handle(w http.ResponseWriter, r *http.Request) {
-	tmpl, err := template.ParseGlob("templates/*")
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
-
-	name := "index.html"
-	if r.URL.Path == "/" {
-		name = "index.html"
-	} else {
-		name = path.Base(r.URL.Path)
-	}
-
-	data := struct {
-		Time time.Time
-	}{
-		Time: time.Now(),
-	}
-
-	if err := tmpl.ExecuteTemplate(w, name, data); err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		fmt.Println("error", err)
-	}
-}
-
 func main() {
-	fmt.Println("http server up at port 8081!")
-	http.Handle(
-		"/static/",
-		http.StripPrefix(
-			"/static/",
-			http.FileServer(http.Dir("static")),
-		),
-	)
-	http.HandleFunc("/", handle)
-	http.ListenAndServe(":8081", nil)
+	fmt.Print("starting a local host on port 8080.")
+	http.Handle("/", http.FileServer(http.Dir("static")))
+	http.ListenAndServe(":8080", nil)
 }
