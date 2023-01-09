78e898cbe799a6ce1acf4dc06309b222c634253e
iyanisse <yaisse.ismaili@ynov.com>
2023-01-09 10:15:18 +0100

    nain

--- localhost.go
+++ localhost.go
@@ -6,8 +6,8 @@ import (
 )
 
 func main() {
-	fmt.Print("starting a local host on port 8080.","\n")
-	fmt.Print("\n","enjoy your game 0w0")
+	fmt.Print("starting a local host on port 8080.", "\n")
+	fmt.Print("\n", "enjoy your game 0w0")
 	http.Handle("/", http.FileServer(http.Dir("static")))
 	http.ListenAndServe(":8080", nil)
 }
