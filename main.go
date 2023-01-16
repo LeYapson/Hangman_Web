94bd7bbbbc4704266edf5c15cd552774075c1d65
iyanisse <yaisse.ismaili@ynov.com>
2023-01-16 11:51:20 +0100

    commentaire

--- main.go
+++ main.go
@@ -7,21 +7,21 @@ import (
 	"text/template"
 )
 
-func main(){
+func main(){ // permet de lancer le local host et charger les page web
 	http.HandleFunc("/", Home)
 	http.HandleFunc("/game", Game)
 	localhost()
 	
 }
-func Home (w http.ResponseWriter, r *http.Request){
+func Home (w http.ResponseWriter, r *http.Request){ // afiche les page
 	Rendertemplate(w, "home")
 }
 
-func Game (w http.ResponseWriter, r *http.Request) {
+func Game (w http.ResponseWriter, r *http.Request) { // Hangman web
 	Rendertemplate(w, "game")
 }
 
-func Rendertemplate(w http.ResponseWriter, tmpl string) {
+func Rendertemplate(w http.ResponseWriter, tmpl string) { // Cherche le rendu de la template dans les fichier
 	t, err :=template.ParseFiles("./templates/" + tmpl + ".page.tmpl")
 	if err != nil {
 		fmt.Println("error")
