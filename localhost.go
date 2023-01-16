377619807267039f2174f5ed3755c3b98076d294
Yapi théau <theau.yapi@ynov.com>
2023-01-16 11:33:07 +0100

    qùgfpfvs

--- localhost.go
+++ localhost.go
@@ -7,7 +7,6 @@ import (
 
 func localhost() {
 	fmt.Print("starting a local host on port 8080.", "\n")
-	fmt.Print("\n", "enjoy your game 0w0")
-	http.Handle("/", http.FileServer(http.Dir("static")))
+	fmt.Print("\n", "enjoy your game 0w0","\n")
 	http.ListenAndServe(":8080", nil)
 }
--- main.go
+++ main.go
@@ -1,7 +1,30 @@
 package main
 
-//import "github.com/sinjin314/hangman-classic"
+import (
+	//"github.com/sinjin314/hangman-classic"
+	"fmt"
+	"net/http"
+	"text/template"
+)
 
 func main(){
+	http.HandleFunc("/", Home)
+	http.HandleFunc("/game", Game)
 	localhost()
+	
+}
+func Home (w http.ResponseWriter, r *http.Request){
+	Rendertemplate(w, "home")
+}
+
+func Game (w http.ResponseWriter, r *http.Request) {
+	Rendertemplate(w, "game")
+}
+
+func Rendertemplate(w http.ResponseWriter, tmpl string) {
+	t, err :=template.ParseFiles("./templates/" + tmpl + ".page.tmpl")
+	if err != nil {
+		fmt.Println("error")
+	}
+	t.Execute(w, nil)
 }
