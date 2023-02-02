01f505392e23b048a0dd32d434035d4a9525e1d7
LeYapson <theau.yapi@ynov.com>
2023-02-02 09:07:02 +0100

    finito

--- controller/htmlfiles.go
+++ controller/htmlfiles.go
@@ -2,5 +2,4 @@ package hangmanweb
 
 var PlayPage = []string{"./view/html/index.html", "./view/html/templates/head.html"}
 var RegisterPage = []string{"./view/html/register.html", "./view/html/templates/head.html"}
-var LeaderBoardPage = []string{"./view/html/leaderboard.html", "./view/html/templates/head.html"}
 var DifficultyPage = []string{"./view/html/difficulty.html", "./view/html/templates/head.html"}
